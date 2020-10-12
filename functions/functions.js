90781acb42d2e9594190c2fa099628f24ae34531
jitesh srivastava <jitesh.srivastava@thoughtworks.com>
2020-10-12 23:49:12 +0530

    Adds init

---
+++ functions/functions.js
@@ -0,0 +1,11 @@
+// function declaration 
+
+function greet() {
+    console.log('Hi from greet')
+}
+
+// function expression
+
+const speak = () => {
+    console.log('hi from speak -- function expression')
+}
