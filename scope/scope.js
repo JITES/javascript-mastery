dcfc0741c9ac50d81210ba5e608a20ee85b3896d
Jitesh Srivastava <jitesh.srivastava@thoughtworks.com>
2021-03-10 22:42:52 +0530

    Added books

--- scope/scope.js
+++ scope/scope.js
@@ -1,9 +1,10 @@
 function foo() {
-  console.log('Hey I am in a');
   var a = 'A';
   function bar() {
     console.log(a);
   }
+  bar();
 }
-
+var a = 'Global A';
 foo();
+// ! bar(); This will give an error because bar`s outer lexical environmenet is foo not global lexical environment
